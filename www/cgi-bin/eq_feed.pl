69a7f5d428cdaab0bc8fef1dda4ab4a6ae26b0f9
Ryan Berckmans <ryanberckmans@gmail.com>
2014-10-20 12:28:24 -0400

    eq_feed.pl, change json output format to array instead of hash, skip items with no auction time remaining, sort only by current_bid_time (ignore add_time)

--- www/cgi-bin/eq_feed.pl
+++ www/cgi-bin/eq_feed.pl
@@ -15,6 +15,7 @@ use JSON;
 use DBI;
 
 require "db.pl";
+require "auction_timing.pl";
 
 my $dbh = get_db();
 
@@ -22,24 +23,30 @@ my $EQ_NEWER_THAN_SECONDS = 60 * 60* 24 * 5;
 
 my $eq_filter = " WHERE (UNIX_TIMESTAMP(now()) - UNIX_TIMESTAMP(add_time)) < $EQ_NEWER_THAN_SECONDS && status != 'picked' ";
 
-my $eq_query = "select id, zone, descr, bid, status, first_bid_time, cur_bid_time, add_time from bid_eq $eq_filter ORDER BY cur_bid_time, add_time";
+my $eq_query = "select id, zone, descr, bid, status, first_bid_time, cur_bid_time, add_time, UNIX_TIMESTAMP(now()) as current_time_unix, UNIX_TIMESTAMP(first_bid_time) as first_bid_time_unix, UNIX_TIMESTAMP(cur_bid_time) as cur_bid_time_unix, UNIX_TIMESTAMP(add_time) as add_time_unix from bid_eq $eq_filter ORDER BY cur_bid_time";
 
 my $sth = $dbh->prepare($eq_query);
 $sth->execute;
 
 
-my %eq = ();
+my @eq = ();
 
-while (my ($id, $zone, $descr, $bid, $status, $first_bid_time, $cur_bid_time, $add_time) = $sth->fetchrow_array) {
-  $eq{ $id } = {
-              'zone' => $zone,
-              'description' => $descr,
-              'currentBid' => $bid,
-              'currentBidTime' => $cur_bid_time,
-            };
+while (my ($id, $zone, $descr, $bid, $status, $first_bid_time, $cur_bid_time, $add_time, $current_time_unix, $first_bid_time_unix, $cur_bid_time_unix, $add_time_unix) = $sth->fetchrow_array) {
+
+  if (auction_seconds_remaining($current_time_unix, $add_time_unix, $cur_bid_time_unix, $first_bid_time_unix) < 1) {
+    next;
+  }
+
+  push(@eq, {
+    'id' => $id,
+    'zone' => $zone,
+    'description' => $descr,
+    'currentBid' => $bid,
+    'currentBidTime' => $cur_bid_time,
+  });
 }
 
-my $json_text = to_json(\%eq);
+my $json_text = to_json(\@eq);
 
 print header('application/json');
 
